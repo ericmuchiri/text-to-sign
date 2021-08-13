05e2fe89864fb300333c339bab550c9471922afd
cto eric <ericmchri@gmail.com>
2021-08-13 19:17:38 +0300

    new improve

---
+++ A2SL/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import nltk
+from django.contrib.staticfiles import finders
+from django.contrib.auth.decorators import login_required
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def about_view(request):
+    return render(request, 'about.html')
+
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # log the user in
+            return redirect('animation')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in user
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('animation')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
---
+++ convert/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Conversion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    search_text = models.CharField(max_length=5000, blank=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.search_text
